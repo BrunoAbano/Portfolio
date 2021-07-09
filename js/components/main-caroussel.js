aded34221f49885128203233614ae1f84578d59f
Brunix <brunix.abanix@gmail.com>
2021-07-09 19:04:15 +0000

    Limitação no scroll

--- js/components/main-caroussel.js
+++ js/components/main-caroussel.js
@@ -1,14 +1,29 @@
 const btnRight = window.document.querySelector('.button-arrow.-right');
 const btnLeft = window.document.querySelector('.button-arrow.-left');
 const elements = window.document.querySelector('.elements');
-let pixels = 100;
+let pixels = 0;
+let min = 1000;
+let max = 0;
 
 btnRight.addEventListener('click', function() {
-    pixels = pixels + 100;
-    elements.style = `transform: translateX(${pixels}px)`;
+    if(max == 0){
+
+    }else{
+        pixels = pixels - 100;
+        elements.style = `transform: translateX(${pixels}px)`;
+        min = min + 100;
+        max = max - 100;
+    }
+
 })
 
 btnLeft.addEventListener('click', function() {
-    pixels = pixels - 100;
-    elements.style = `transform: translateX(${pixels}px)`;
+    if(min == 0){
+
+    }else{
+        pixels = pixels + 100;
+        elements.style = `transform: translateX(${pixels}px)`;
+        min = min - 100;
+        max = max + 100;
+    }
 })
